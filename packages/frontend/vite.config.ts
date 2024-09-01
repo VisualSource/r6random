a1611f9f9197f15e4b31356c1bf938e7e7e4db35
VisualSource <collin_blosser@yahoo.com>
2024-09-01 17:49:07 -0400

    fix output dir

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from "node:path"
 
 const ID = "/r6random/"
-const OUTDIR = `../../../dist${ID}`
+const OUTDIR = `../../../../dist${ID}`
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {

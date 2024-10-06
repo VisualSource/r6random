c309f8168202adac47defebd4a1c86ad06ea7803
VisualSource <collin_blosser@yahoo.com>
2024-10-06 15:09:14 -0400

    add skopos

--- packages/frontend/src/lib/hooks/useSelectedOperators.ts
+++ packages/frontend/src/lib/hooks/useSelectedOperators.ts
@@ -1,8 +1,14 @@
 import useLocalStorageState from "use-local-storage-state";
+import ops from "../../assets/operators.json";
 import type { Team } from "@/Router";
+
+const getDefaultSelected = (team: Team) => {
+	if (team === "attackers") return ops.attackers.map(e => e.id);
+	return ops.defenders.map(e => e.id);
+}
 
 export const useSelectedOperators = (team: Team) => {
 	return useLocalStorageState<string[]>(`r6r.${team}`, {
-		defaultValue: [],
+		defaultValue: getDefaultSelected(team),
 	});
 };
